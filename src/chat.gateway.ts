b977ad6dd25a0fe772a896104df65d3a93e46640
aminwhat <m.aminwhat@gmail.com>
2022-09-22 21:54:23 +0330

    add basic socket

---
+++ src/chat.gateway.ts
@@ -0,0 +1,19 @@
+import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketServer,
+} from '@nestjs/websockets/decorators';
+
+@WebSocketGateway(80, { namespace: 'chat' })
+export class ChatGateway {
+  @WebSocketServer()
+  server;
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): void {
+    // also can use (client, data) for parameters
+    this.server.emit('message', message);
+    console.log(typeof this.server);
+  }
+}
